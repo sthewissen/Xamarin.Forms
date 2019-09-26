896d2dab84f05791cfaa1131099b15024e9a7206
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-09-26 12:37:53 +0200

    Added sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7678.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[NUnit.Framework.Category(UITestCategories.CarouselView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 7678, "[iOS] CarouselView binded to a ObservableCollection and add Items later, crash", PlatformAffected.iOS)]
+	public class Issue7678Ios : TestContentPage
+	{
+		public Issue7678Ios()
+		{
+			Title = "Issue 7678";
+			BindingContext = new Issue7678IosViewModel();
+		}
+
+		protected override void Init()
+		{
+			var layout = new Grid();
+
+			var itemsLayout =
+				new LinearItemsLayout(ItemsLayoutOrientation.Horizontal)
+				{
+					SnapPointsType = SnapPointsType.MandatorySingle,
+					SnapPointsAlignment = SnapPointsAlignment.Center
+				};
+
+			var carouselView = new CarouselView
+			{
+				ItemsLayout = itemsLayout,
+				ItemTemplate = GetCarouselTemplate()
+			};
+
+			carouselView.SetBinding(ItemsView.ItemsSourceProperty, "Items");
+
+			layout.Children.Add(carouselView);
+
+			Content = layout;
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+
+			await ((Issue7678IosViewModel)BindingContext).LoadItemsAsync();
+		}
+
+		internal DataTemplate GetCarouselTemplate()
+		{
+			return new DataTemplate(() =>
+			{
+				var grid = new Grid();
+
+				var info = new Label
+				{
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.Center,
+					Margin = new Thickness(6)
+				};
+
+				info.SetBinding(Label.TextProperty, new Binding("Name"));
+
+				grid.Children.Add(info);
+
+				var frame = new Frame
+				{
+					Content = grid,
+					HasShadow = false
+				};
+
+				frame.SetBinding(BackgroundColorProperty, new Binding("Color"));
+
+				return frame;
+			});
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue7678IosViewModel : BindableObject
+	{
+		ObservableCollection<CarouselData> _items;
+
+		public ObservableCollection<CarouselData> Items
+		{
+			get { return _items; }
+			set
+			{
+				_items = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public async Task LoadItemsAsync()
+		{
+			Items = new ObservableCollection<CarouselData>();
+
+			await Task.Delay(500);
+
+			var random = new Random();
+
+   			for (int n = 0; n < 5; n++)
+			{
+				Items.Add(new CarouselData
+				{
+					Color = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)),
+					Name = $"{n + 1}"
+				});
+			}
+		}
+	}
+}
