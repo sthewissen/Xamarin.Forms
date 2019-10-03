5fe0da3c0ed5104cc5cb2419ace84d9f1be1f2ab
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-10-03 16:26:31 +0200

    Fix 7249 - Fix Switch Thumb wrong color on Android

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7249.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 7249, "(Android) Wrong color on Slider", PlatformAffected.Android)]
+	public class Issue7249 : TestContentPage
+	{
+		public Issue7249()
+		{
+			Title = "Issue 7249";
+		}
+
+		protected override void Init()
+		{
+			var layout = new StackLayout
+			{
+				Padding = new Thickness(12)
+			};
+
+			var instructions = new Label
+			{
+				Text = "Toggle the first Switch and verify that the color of the Thumb is equal to the Thumb color of the second Switch."
+			};
+
+			var switch1 = new Switch
+			{
+				HorizontalOptions = LayoutOptions.Start,
+				IsToggled = false
+			};
+
+			var switch2 = new Switch
+			{
+				HorizontalOptions = LayoutOptions.Start,
+				IsToggled = true
+			};
+
+			layout.Children.Add(instructions);
+			layout.Children.Add(switch1);
+			layout.Children.Add(switch2);
+
+			Content = layout;
+		}
+	}
+}
